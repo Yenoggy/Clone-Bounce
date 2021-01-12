fab71523907f79a23b1711bb68b0eb267fa92374
Yenoggy <yenoggye@yandex.ru>
2021-01-12 20:13:54 +0300

    Update launcher

--- launcher.py
+++ launcher.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from win32api import GetSystemMetrics
+import webbrowser
 
 
 ScreenX = GetSystemMetrics(0)
@@ -10,6 +11,7 @@ class Main:
 
     def __init__(self, master):
 
+        # Цыганские фокусы для окна
         self.Hidden = 0
         self.window = master
         self.window.title("Clone Bounce")
@@ -21,16 +23,19 @@ class Main:
         self.Startfr = Frame(master, width=285, bg='#151515')
         self.Startfr.pack(fill=Y, side=LEFT)
 
+        # Заголовок игры
         self.game_title = Label(self.Startfr, text='Clone Bounce', font=('Arial', 30), cursor='hand2')
         self.game_title.config(bg='#151515', activebackground='#151515', fg='grey')
         self.game_title.place(x=20, y=50)
 
-        self.Startbtn = Button(self.Startfr, text='SINGLE PLAY', border=0, command=self.btnStart, cursor='hand2', font=17)
-        self.Startbtn.config(bg='#202020', activebackground='#151515', activeforeground='grey', fg='grey')
-        self.Startbtn.bind("<Enter>", self.inStart)
-        self.Startbtn.bind("<Leave>", self.outStart)
-        self.Startbtn.place(x=89, y=140)
+        # Кнопка запуска синглплеера
+        self.StartbtnSingle = Button(self.Startfr, text='SINGLE PLAY', border=0, command=self.btnStartSingle, cursor='hand2', font=17)
+        self.StartbtnSingle.config(bg='#202020', activebackground='#151515', activeforeground='grey', fg='grey')
+        self.StartbtnSingle.bind("<Enter>", self.inStart)
+        self.StartbtnSingle.bind("<Leave>", self.outStart)
+        self.StartbtnSingle.place(x=89, y=140)
 
+        # Кнопка запуска мультиплеера
         self.Startbtn_mp = Button(self.Startfr, text='MULTIPLAYER', border=0, command=self.btnStart, cursor='hand2',
                                font=17)
         self.Startbtn_mp.config(bg='#202020', activebackground='#151515', activeforeground='grey', fg='grey')
@@ -38,6 +43,7 @@ class Main:
         self.Startbtn_mp.bind("<Leave>", self.outStart_mp)
         self.Startbtn_mp.place(x=86, y=200)
 
+        # Выход из лаунчера
         self.Exitlbl = Label(self.Startfr, text='Exit', font=('Arial', 12), cursor='hand2')
         self.Exitlbl.bind('<Button-1>', self.close)
         self.Exitlbl.bind('<Enter>', self.inStart_ex)
@@ -45,13 +51,30 @@ class Main:
         self.Exitlbl.config(bg='#151515', activebackground='#151515', activeforeground='grey', fg='grey')
         self.Exitlbl.place(x=125, y=283)
 
-    def btnStart(self):
+        # Титры
+        self.by = Label(self.Startfr, text='By CR4CKKER, GaLeon and YENOGGY', font=('Arial', 8))
+        self.by.config(bg='#151515', activebackground='#151515', activeforeground='grey', fg='grey')
+        self.by.place(x=45, y=310)
+        self.by.bind('<Button-1>', self.github)
+        self.by.bind('<Enter>', self.inBy)
+        self.by.bind('<Leave>', self.outBy)
+
+
+    # Запуск сингла
+    def btnStartSingle(self):
         import main
         self.window.destroy()
 
+    # Закрытие окна
     def close(self, event):
         self.window.destroy()
 
+    # Ссылка на гит
+    def github(self, event):
+        webbrowser.open('https://github.com/Yenoggy/Clone-Bounce', new=2)
+
+
+    # Эффекты
     def inStart(self, event):
         self.Startbtn.config(bg='#202020', activebackground='#151515', fg='white')
 
@@ -70,7 +93,13 @@ class Main:
     def outStart_ex(self, event):
         self.Exitlbl.config(bg='#151515', activebackground='#151515', fg='grey', font='Arial 12')
 
+    def inBy(self, event):
+        self.by.config(bg='#151515', activebackground='#151515', fg='white', font='Arial 8 underline')
 
+    def outBy(self, event):
+        self.by.config(bg='#151515', activebackground='#151515', fg='grey', font='Arial 8')
+
+# Запуск лаунчера
 if __name__ == '__main__':
     app = Main(screen)
     screen.mainloop()
